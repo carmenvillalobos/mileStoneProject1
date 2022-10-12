a44dd9913c6992f913a5da5cc5586e41f2df396a
Carmen Villalobos <carmenvillalobos@Carmens-MBP.attlocal.net>
2022-10-11 20:35:27 -0700

    Added a countdown timer and removed the moving white square once the time is up

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+// adding a point for every time you click on the white square
 var score = 0;
 
 function addToScore(amount){
     score = score + amount
     document.getElementById("score").innerHTML = score
 }
+
+// countdown timer for game length
+let timer = 21
+function decreaseTimer() {
+    // decrease the timer length by 1
+    if (timer > 0) {
+        setTimeout(decreaseTimer, 1000)
+        timer--
+        document.getElementById("timer").innerHTML = `Timer: ${timer}`
+    }
+
+    // display text that says the game is over once the time reaches 0
+    if(timer == 0) {
+        document.getElementById("timer").innerHTML = "Time's up!"
+        document.getElementById("whiteSquare").style.display = "none"
+    }
+}
+
+// invoke the function to display the countdown timer
+decreaseTimer()
+
